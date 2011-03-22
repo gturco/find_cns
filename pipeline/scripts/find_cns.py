dec053ac9be3d13061466f03c08f06ba6e8c0cc1
gina <gturco88@gmail.com>
2011-03-21 17:05:27 -0700

    eidit main

--- pipeline/scripts/find_cns.py
+++ pipeline/scripts/find_cns.py
@@ -335,7 +335,7 @@ def get_masked_fastas(bed):
         fh.close()
     return fastas
 
-def main(qbed, sbed, pairs_file, mask='F', ncpu=8):
+def main(qbed, sbed, fbed, pairs_file, mask='F', ncpu=8):
     """main runner for finding cnss"""
     pool = Pool(options.ncpu)
 
