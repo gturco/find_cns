328b220bd48b082e054d52540c55b38fc9934c01
Gina Turco <gturco@uci.edu>
2011-04-05 17:01:56 -0700

    made e value two dec float

--- pipeline/scripts/find_cns.py
+++ pipeline/scripts/find_cns.py
@@ -331,7 +331,7 @@ def main(qbed, sbed, pairs_file, mask='F', ncpu=8):
 
     bl2seq = "~/src/blast-2.2.25/bin/bl2seq " \
            "-p blastn -D 1 -E 2 -q -2 -r 1 -G 5 -W 7 -F %s " % mask + \
-           " -e %(e_value)d -i %(sfasta)s -j %(qfasta)s \
+           " -e %(e_value).2f -i %(sfasta)s -j %(qfasta)s \
              -I %(sstart)d,%(sstop)d -J %(qstart)d,%(qstop)d | grep -v '#' \
             | grep -v 'WARNING' | grep -v 'ERROR' "
 
