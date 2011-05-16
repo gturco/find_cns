5465006cecf6186732c2b957373c6156413345c6
Gina Turco <gturco@uci.edu>
2011-05-16 13:01:28 -0700

    works with pickle file now need ncpu to work next :P

--- pipeline/scripts/blast_interval.py
+++ pipeline/scripts/blast_interval.py
@@ -7,6 +7,7 @@ import os
 import os.path as op
 import commands
 from bx.intervals.intersection import Interval, IntervalTree
+from find_cns_regions import get_pair
 
 
 def find_median_interval(file_path):
@@ -32,7 +33,7 @@ def find_median_interval(file_path):
 # blast_cmd
 # bed.cds_fasta()
 
-def parse_blast(blast_str, cns_dict):
+def parse_blast(blast_str, qfeat):
   """takes a blast file and cns_pair and sees if the query cns intersects with 
   any of the cns found"""
   scns_inteval = IntervalTree()
@@ -48,8 +49,8 @@ def parse_blast(blast_str, cns_dict):
     s_end = max(int(s_start), int(s_end))
     scns_inteval.insert_interval(Interval(s_start, s_end))
 
-  q_start = min(int(cns_dict['start']), int(cns_dict['end']))
-  q_end = max(int(cns_dict['start']), int(cns_dict['end'])) 
+  q_start = min(int(qfeat['start']), int(qfeat['end']))
+  q_end = max(int(qfeat['start']), int(qfeat['end'])) 
   intersecting_cns = scns_inteval.find(q_start, q_end)
   return intersecting_cns
   
@@ -85,49 +86,57 @@ def get_fastas(bed, masked = True):
         fh.close()
       return fastas
 
-def main(cns_bed, ortho_bed, cns_dict, qpad, spad, blast_path, mask):
+def main(cns_bed, ortho_bed, pairs_file, qpad, spad, blast_path, mask):
   "imput a cns_dict and otholog_dict, cns_bed "
-  
+  fcns = sys.stdout
+  print >> fcns, "#cns_start, cns_stop, interval"
   bl2seq = "%s " % blast_path + \
           "-p blastn -D 1 -E 2 -q -2 -r 1 -G 5 -W 7 -F %s " % mask + \
           " -e %(e_value).2f -i %(qfasta)s -j %(sfasta)s \
              -I %(qstart)d,%(qstop)d -J %(sstart)d,%(sstop)d | grep -v '#' \
            | grep -v 'WARNING' | grep -v 'ERROR' "
-
+           
   qfastas = get_fastas(cns_bed, True)
   sfastas = get_fastas(ortho_bed, False)
-
-  def get_cmd(sfeat, qfeat, qpad, spad, mask):
-        #if qfeat['accn'] != "Bradi4g01820": return None
-        #print >>sys.stderr, qfeat, sfeat
-        
-        sfasta = sfastas[sfeat['seqid']]
-        #qfasta = '/Users/gturco/trobble_shot/rice_v6_split/4.fasta'
-        qfasta = qfastas[qfeat['seqid']]
-        
-
-        qstart, qstop = max(qfeat['start'] - qpad, 1), qfeat['end'] + qpad
-        sstart, sstop = max(sfeat['start'] - spad, 1), sfeat['end'] + spad
-
-        m = qstop - qstart
-        n = sstop - sstart
-      
-        e_value = m*n*(2**(-28.51974)) # bit score above 15/15 noise
-        assert e_value > 0
-
-        cmd = bl2seq % dict(qfasta=qfasta, sfasta=sfasta, qstart=qstart,
-                            sstart=sstart, qstop=qstop, sstop=sstop, e_value=e_value)
-        return cmd
-        
-  sfeat = ortho_bed.accn('GRMZM2G094328')
-  x = get_cmd(sfeat,cns_dict, qpad, spad, 'T')
-  results = commands.getoutput(x)
-  interval = parse_blast(results, cns_dict) 
-  if len(interval) >= 1:
-     print cns_dict['start'], cns_dict['end'], interval
+  pairs = [True]
+  _get_pair_gen = get_pair(pairs_file ,'pck', ortho_bed, cns_bed)
   
+  def get_pair_gen():
+    try: return _get_pair_gen.next()
+    except StopIteration: return None
+      
+  while any(pairs):
+    pairs = get_pair_gen()
+    qfeat, sfeat = pairs
+    if pairs is None: return None
 
-
+    def get_cmd(qfeat, sfeat, qpad, spad, mask):
+      if pairs is None: return None
+      
+      sfasta = sfastas[sfeat['seqid']]
+      qfasta = qfastas[qfeat['seqid']]
+      
+      qstart, qstop = max(qfeat['start'] - qpad, 1), qfeat['end'] + qpad
+      sstart, sstop = max(sfeat['start'] - spad, 1), sfeat['end'] + spad
+      
+      m = qstop - qstart
+      n = sstop - sstart
+      
+      e_value = m*n*(2**(-28.51974)) # bit score above 15/15 noise
+      assert e_value > 0
+      
+      cmd = bl2seq % dict(qfasta=qfasta, sfasta=sfasta, qstart=qstart,
+                          sstart=sstart, qstop=qstop, sstop=sstop, e_value=e_value)
+      return cmd
+      
+    #cmds = [c for c in map(get_cmd(), [l for l in pairs if l] if c]
+    cmd = get_cmd(qfeat, sfeat, qpad, spad, mask)
+    results = commands.getoutput(cmd)
+    if not results.strip(): continue 
+    interval = parse_blast(results, qfeat)
+    if len(interval) >= 1:
+      print >> fcns, '{0},{1},{2}'.format(qfeat['start'], qfeat['end'], interval)
+  
 if __name__ == "__main__":
     import optparse
     parser = optparse.OptionParser("usage: %prog [options] ")
@@ -139,16 +148,15 @@ if __name__ == "__main__":
     parser.add_option("--qpad", dest="qpad", type='int', default=12000, help="how far from the end of the query gene to look for cnss")
     parser.add_option("--spad", dest="spad", type='int', default=26000, help="how far from the end of the subject gene to look for cnss")
     parser.add_option("--blast_path", dest="blast_path", type='string', help="path to bl2seq")
+    parser.add_option("--pairs_file", dest="pairs", help='pairs file containg cns_start,cns_end, cns_seqid and its ortholog')
     (options, _) = parser.parse_args()
     
-    # if not (options.qfasta and options.sfasta and options.sbed and options.qbed):
-    #     print 'hi'
-    #     sys.exit(parser.print_help())
+    if not (options.qfasta and options.sfasta and options.sbed and options.qbed, options.pairs):
+        sys.exit(parser.print_help())
         
-    print options.qbed, options.qfasta, options.blast_path, options.mask
-    cns_dict = {'start':31210231, 'end':31210254, 'seqid':'4'}
-    qbed = Bed(options.qbed, options.qfasta); qbed.fill_dict()
-    sbed = Bed(options.sbed, options.sfasta); sbed.fill_dict()
+    #cns_dict = {'start':31210231, 'end':31210254, 'seqid':'4'}
+    # qbed = Bed(options.qbed, options.qfasta); qbed.fill_dict()
+    # sbed = Bed(options.sbed, options.sfasta); sbed.fill_dict()
     assert options.mask in 'FT'
 
-    main(qbed, sbed, cns_dict, options.qpad, options.spad, options.blast_path, options.mask)
+    main(qbed, sbed, options.pairs, options.qpad, options.spad, options.blast_path, options.mask)
