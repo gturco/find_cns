a63efb9cf1d6a3e7de7396db4ec795dcddf644c5
Gina Turco <gturco@uci.edu>
2011-06-01 16:40:28 -0700

    redirected get_pair in assign to one with pck

--- pipeline/scripts/assign.py
+++ pipeline/scripts/assign.py
@@ -1,143 +1,194 @@
-from flatfeature import Bed
 import collections
-import numpy
-from numpy import array
+from flatfeature import Bed
 import sys
 import os
 import os.path as op
 sys.path.insert(0, os.path.dirname(__file__))
-from find_cns import get_pair
+from find_cns_regions.py import get_pair
+import pickle 
 
-def assign_url(scns, sseqid, qcns, qseqid, qfeat, pairsfile, sbed, qbed, sorg, qorg, padding,
-               base = "http://synteny.cnr.berkeley.edu/CoGe/GEvo.pl?prog=blastn&autogo=1&"):
-    "lines up coge based on the cns postion"
-    sfeat = get_homeolog(qfeat,pairsfile, sbed, qbed)
-    inside = "dsgid1={5}&chr1={0}&x1={1}&dr1up={7}&dr1down={7}&dsgid2={6}&chr2={2}&x2={3}&dr2up={7};dr2down={7}&"\
-             "accn3={4};dr3up=15000;dr3down=15000;num_seqs=3;hsp_overlap_limit=0;hsp_size_limit=0".format(sseqid, scns, qseqid, qcns, sfeat, sorg, qorg, padding)
-    url = base + inside
-    return url
+#same as brents assign just added coge links.. may want to add otption for links
+def cns_id(cns_dict):
+    c = cns_dict
 
-def get_homeolog(qfeat,pairsfile, sbed, qbed):
-    for region, sregion in get_pair(pairsfile, 'pck', sbed, qbed):
-        if region['sfeat'] == qfeat[3]:
-            return region['ORG2_qfeat']
-           
+    return "|".join(map(str,
+           (c['qaccn'], c['qchr'], c['qstart'], c['qstop'], c['qstrand'],
+            c['saccn'], c['schr'], c['sstart'], c['sstop'], c['sstrand'])))
+
+def cns_link(cns_dict, qorg, sorg, pad, base=" http://synteny.cnr.berkeley.edu/CoGe/GEvo.pl?prog=blastn&autogo=1&"):
+    d = cns_dict.copy()
+    d['qorg'] = qorg
+    d['sorg'] = sorg
+    
+    url = "dsid1=%(qorg)s&chr1=%(qchr)s&x1=%(qstart)s&dr1up=%(pad)s&dr1down=%(pad)s&\
+dsid2=%(sorg)s;chr2=%(schr)s;x2=%(sstart)s;dr2up=%(pad)s;dr2down=%(pad)s;num_seqs=2;hsp_overlap_limit=0;hsp_size_limit=0"
+    return base + (url % d) 
+    
+
+
 def get_cns_dict(cnsfile):
     cnss = collections.defaultdict(list)
     for line in open(cnsfile):
-        if line[0] == "#":continue
+        if line[0] == "#":
+            continue
         line = line.rstrip().split(",")
-        qaccn, qchr, saccn, saccn_l, saccn_r, schr = line[0:6]
-        cnslocs = map(int, line[6:])
+        qchr, qaccn, schr, saccn = line[:4]
+
+        cnslocs = map(int, line[4:])
         if len(cnslocs) % 4: raise
 
         for i in range(0, len(cnslocs), 4):
             key = (qchr, schr, tuple(cnslocs[i:i + 4]))
-            cnss[key].append((qaccn, saccn, saccn_l, saccn_r))
+            cnss[key].append((qaccn, saccn))
     return cnss
-
-    
-def cns_fmt_dict(cns, qfeat, saccn, saccn_l, saccn_r):
-    # saccn, schr, qaccn, qaccnL, qaccnR, qstart, qstop,saccn, schr, sstart, sstop,link
-    d = dict(saccn=saccn, saccnL=saccn_l,saccnR=saccn_r,schr=cns.schr,
-        sstart=cns.sstart, sstop=cns.sstop,
-        qaccn=qfeat['accn'], qchr=qfeat['seqid'],
-        qstart=cns.qstart, qstop=cns.qstop)
-    return d
-    
  
 class CNS(object):
     __slots__ = ("qchr", "schr", "qstart", "qstop", "sstart", "sstop")
     def __init__(self, cnsinfo):
-        self.qchr, self.schr, (self.qstart, self.qstop, self.sstart, self.sstop) = cnsinfo  
-        
-def make_pair_maps(pair_file, fmt, qbed, sbed):
-    """make dicts of q => s and s => q"""
-    qmap_tuple = []
-    for pair in get_pair(pair_file,fmt, qbed, sbed):
-        if pair is None: break
-        (sname, qname) = pair
-        qmap_tuple.append((qname,sname))
-        qmap_tuple.append((sname,qname))
-    return qmap_tuple
-#         
-# def get_nearby_features(feat, bed, p0, p1):
-#     "grabs all features inbtween two postions (chr#, start, stop)"
-#     if p0 < p1:
-#         inters = bed.get_features_in_region(feat['seqid'], p0, p1)
-#     else:
-#         inters = bed.get_features_in_region(feat['seqid'], p1, p0)
-#     return [f for f in inters if f["accn"] != feat["accn"]]   
- 
+        self.qchr, self.schr, (self.qstart, self.qstop, self.sstart, self.sstop) = cnsinfo
+
+def cns_fmt_dict(cns, qfeat, sfeat):
+    # qaccn, qchr, qstart, qstop, qstrand, saccn, schr, sstart, sstop, sstrand, link
+    d = dict(qaccn=qfeat['accn'], qchr=qfeat['seqid'],
+        qstart=cns.qstart, qstop=cns.qstop, qstrand=qfeat['strand'],
+        saccn=sfeat['accn'], schr=sfeat['seqid'],
+        sstart=cns.sstart, sstop=cns.sstop,
+        sstrand=sfeat['strand'])
+    return d
 
 
-def nearest_feat(feats, cns_start, cns_stop):
-    "imputs:a list of feature start and stop postions and a cns start or stop postion\
-     output: the feature start or stop postion that is closest to the cbs "
-    dist = array([[abs(x - cns_start),abs(y - cns_start)] for x,y in feats])
-    params = [[(x , cns_start),(y , cns_start),(x , cns_stop),(y , cns_stop)] for x,y in feats]
-    dist_min = numpy.unravel_index(dist.argmin(), dist.shape)
-    #params_row = params[dist_min[0]]
-    #p0, p1 =  feats[dist_min[1]]
-    return dist_min[0]
+def get_start_stops(feat, cns):
+    """
+    return a range dependent on the position of the cns
+    relative to the feature
+    """
+    if cns[0] > cns[1]: cns = cns[1], cns[0]
+    if feat['start'] < cns[0] and feat['end'] > cns[1]:
+        # intronicns cnsns:
+        return cns[0], cns[1]
+    featm = (feat['start'] + feat['end']) / 2.
+    cnsm = (cns[0] + cns[1]) /2.
+    if featm < cnsm:
+        return min(feat['end'], cns[0]), max(feat['end'], cns[0])
+    return sorted([cns[1], feat['start']])
     
-def same_chr_feat(feat_list, qbed, cns):
-    f_list = []
-    for f in feat_list:
-        if f['seqid'] == cns.qchr:
-            f_list.append(f)    
-    return f_list[0]
     
 
-def assign(cnsdict, qbed, qpair_map):
-    "finds the nearest qfeat for each duplicat cns qstart qstop, sstart, sstop pos"
-    
+def get_nearby_features(feat, cns_start_stop, bed):
+    p0, p1 = get_start_stops(feat, cns_start_stop)
+    inters = bed.get_features_in_region(feat['seqid'], p0, p1)
+    return [f for f in inters if f["accn"] != feat["accn"]]
+
+
+def assign(cnsdict, qbed, sbed, qpair_map, spair_map):
+
     for cnsinfo, accns in cnsdict.iteritems():
         cns = CNS(cnsinfo)
-        qfeats = []
-        for qaccn, saccn, saccn_l, saccn_r in accns:
-            left_retained = ([l for (k,l) in qpair_map if saccn_r[:-1] in k])
-            left_retained_one = same_chr_feat(left_retained, qbed, cns)
-            right_retained = [l for (k,l) in qpair_map if saccn_l[1:] in k]
-            right_retained_one = same_chr_feat(right_retained, qbed, cns)
-            left_feat,right_feat = qbed.accn(left_retained_one), qbed.accn(right_retained_one)
-            qfeat1 = qbed.accn(qaccn)
-            h_feats = [left_retained_one['accn'], right_retained_one['accn'], qfeat1['accn']]
-            homeolog_feats = [(left_retained_one['start'],left_retained_one['end']), (right_retained_one['start'],right_retained_one['end']), (qfeat1['start'], qfeat1['end'])]
-            dist_min = nearest_feat(homeolog_feats, cns.qstart, cns.qstop)
-            if dist_min < 2: continue
-            try:
-                qbed_value = qbed.d[qaccn]
-                qfeats.append((qbed_value,saccn,saccn_l,saccn_r))
-            except KeyError:
-                print >>sys.stderr, "skipped non top-level features:", qaccn , saccn
-                raise
-        if len(qfeats) == 0: continue
-        qfeat_start_stops = [(qfeat['start'], qfeat['end']) for qfeat, saccn,saccn_l,saccn_r in qfeats]
-            #print qfeat_start_stops
-        pos = nearest_feat(qfeat_start_stops, cns.qstart, cns.qstop)
-        qfeat, saccn,saccn_l,saccn_r = qfeats[pos]
-        yield cns, saccn, saccn_l, saccn_r, qfeat
 
-        
+        def dist_sort(a, b):
+            # sort features by distance to a given cns.
+            qda = get_start_stops(a[0], (cns.qstart, cns.qstop))
+            qdb = get_start_stops(b[0], (cns.qstart, cns.qstop))
+            qda = abs(qda[1] - qda[0])
+            qdb = abs(qdb[1] - qdb[0])
+
+            sda = get_start_stops(a[1], (cns.sstart, cns.sstop))
+            sdb = get_start_stops(b[1], (cns.sstart, cns.sstop))
+            sda = abs(sda[1] - sda[0])
+            sdb = abs(sdb[1] - sdb[0])
+            return cmp(sda + qda, sdb + qdb)
+
+
+        feats = [] 
+        for qaccn, saccn in accns:
+            try:
+                feats.append((qbed.d[qaccn], sbed.d[saccn]))
+            except KeyError:
+                print >>sys.stderr, "skipped non top-level features:", qaccn, saccn
+                raise
+                #continue
+        feats.sort(cmp=dist_sort)
+
+        for qfeat, sfeat in feats:
+            qint = get_nearby_features(qfeat, (cns.qstart, cns.qstop), qbed)
+            sint = get_nearby_features(sfeat, (cns.sstart, cns.sstop), sbed)
+
+            if len(qint) + len(sint) > 3: continue
+
+            nqretained = sum(1 for q in qint if q['accn'] in qpair_map)
+            nsretained = sum(1 for s in sint if s['accn'] in spair_map)
             
-def main(cnsfile, qbed_file, sbed_file, pairsfile, pck, qorg, sorg, padding):
+            # look for intervening retained
+            # a-b-CNS
+            # a-z-CNS-b
+            # so it was assinged to a, but b intervenes.
+            if nqretained + nsretained > 0: continue
+
+            yield cns, qfeat, sfeat
+            # the first 1 had to be the closest...
+            break
+        
+        
+def main(cnsfile, qbed_file, sbed_file, pairsfile, pairs_fmt, qorg, sorg, pad):
+    qcns_file = qbed_file.replace(".bed", "_cns.gff")
+    assert qcns_file != qbed_file
+    qcns_gff = open(qcns_file, 'w')
+    print >>qcns_gff, "##gff-version 3"
+    if sbed_file != qbed_file:
+        scns_file = sbed_file.replace(".bed", "_cns.gff")
+        assert scns_file != sbed_file
+        scns_gff = open(scns_file, 'w')
+        print >>scns_gff, "##gff-version 3"
+    else:
+        scns_gff = qcns_gff
+    
     qbed = Bed(qbed_file); qbed.fill_dict()
     sbed = Bed(sbed_file); sbed.fill_dict()
+
+
     cnsdict = get_cns_dict(cnsfile)
-    qpair_map = make_pair_maps(pairsfile, 'pair', qbed, sbed)
+    qpair_map, spair_map = make_pair_maps(pairsfile, pairs_fmt, qbed, sbed)
     out = sys.stdout
-    
-    fmt = "%(saccn)s,%(saccnL)s,%(saccnR)s,%(schr)s,%(sstart)i,%(sstop)i," + \
-                     "%(qaccn)s,%(qchr)s,%(qstart)i,%(qstop)i,%(link)s" 
-                     
+
+    fmt = "%(cns_id)s,%(qaccn)s,%(qchr)s,%(qstart)i,%(qstop)i,%(qstrand)s," + \
+                       "%(saccn)s,%(schr)s,%(sstart)i,%(sstop)i,%(sstrand)s,%(link)s"
+
     print >>out, "#" + fmt.replace("%(","").replace(")s","").replace(")i","")
-    for cns, saccn, saccn_l, saccn_r, qfeat in assign(cnsdict, qbed, qpair_map): 
-        d = cns_fmt_dict(cns, qfeat, saccn, saccn_l, saccn_r)
-        d['link'] = assign_url(cns.sstart, cns.schr, cns.qstart, cns.qchr,qfeat, pck, sbed, qbed, sorg, qorg, padding)
+    for cns, qfeat, sfeat in assign(cnsdict, qbed, sbed, qpair_map, spair_map):
+        d = cns_fmt_dict(cns, qfeat, sfeat)
+        d['cns_id'] = cns_id(d)
+        if d['sstop'] < d['sstart']:
+            d['sstop'], d['sstart'] = d['sstart'], d['sstop']
+        d['link'] = cns_link(d, qorg, sorg, pad)
         print >>out, fmt % d
-        
-        
+        write_gff(d, qcns_gff, scns_gff)
+
+def write_gff(d, qcns_gff, scns_gff):
+
+    qfmt = \
+        "%(qchr)s\t.\tcns\t%(qstart)i\t%(qstop)i\t.\t%(qstrand)s\t.\tID=q__%(cns_id)s;qaccn=%(qaccn)s;saccn=%(saccn)s"
+    sfmt = \
+        "%(schr)s\t.\tcns\t%(sstart)i\t%(sstop)i\t.\t%(sstrand)s\t.\tID=s__%(cns_id)s;qaccn=%(qaccn)s;saccn=%(saccn)s"
+
+    print >>qcns_gff, qfmt %d
+    print >>scns_gff, sfmt %d
+
+def make_pair_maps(pair_file, fmt, qbed, sbed):
+    """
+    make dicts of q => s and s => q
+    """
+    qmap = collections.defaultdict(list) # key is query, value is a list of subject hits
+    smap = collections.defaultdict(list)
+    print >>sys.stderr, "pair file:", pair_file
+    for pair in get_pair(pair_file, fmt, qbed, sbed):
+        if pair is None: break
+        (qname, sname) = pair
+        qmap[qname].append(sname)
+        smap[sname].append(qname)
+    return qmap, smap
+
+
+
 DEBUG=False
 
 if __name__ == "__main__":
@@ -148,19 +199,19 @@ if __name__ == "__main__":
     parser.add_option("--sbed", dest="sbed", help="bed file of the subject")
     parser.add_option("--cns", dest="cns", help="path to the cns file created by find_cns.py")
     parser.add_option("--pairs", dest="pairs", help="path pairs file")
-    choices = ("dag", "cluster", "pair", "qa", "pck")
+    choices = ("dag", "cluster", "pair", "qa")
     parser.add_option("--pair_fmt", dest="pair_fmt", default='dag',
                       help="format of the pairs, one of: %s" % str(choices),
                       choices=choices)
     parser.add_option("--qorg", dest="qorg", type="int", help="dsid number in coge for the query (same as export to bed input)")
     parser.add_option("--sorg", dest="sorg", type="int", help="dsid number in coge for the subject (same as export to bed input)")
-    parser.add_option("--pad", dest="pad", type='int', default=12000,
-                      help="how far from the end of each gene to set the padding for the link of the cnss")
-    parser.add_option("--pck", dest="pck", help="pairs_file for pck/cns/region")
+    parser.add_option("--pad", dest="pad", type="int", default=15000, help="padding for creating the links")
+
     (options, _) = parser.parse_args()
 
-    res = main(options.cns, options.qbed, options.sbed, options.pairs, options.pck, options.qorg, options.sorg, options.pad)
+    if not (options.qbed and options.sbed and options.cns and options.pairs):
+        sys.exit(parser.print_help())
 
-    
-#if __name__ == "__main__":
-#    main('/Users/gturco/rice_v6_cns_res/04_08_10/test_mine/testassign.txt', '/Users/gturco/rice_v6_cns_res/04_08_10/test_org/rice_v6.bed','/Users/gturco/rice_v6_cns_res/04_08_10/test_org/rice_v6.bed', '/Users/gturco/rice_v6_cns_res/04_08_10/test_org/rice_v6_rice_v6.pairs.txt', '/Users/gturco/rice_v6_cns_res/04_08_10/test_mine/rice_v6_rice_v6.pairs.pck', '9109', '9109', 1000)
+    res = main(options.cns, options.qbed, options.sbed,  options.pairs, options.pair_fmt, options.qorg, options.sorg, options.pad )
+
+
