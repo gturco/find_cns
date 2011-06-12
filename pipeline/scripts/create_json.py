230dfed718d5b08e2112f208d8f2550779ebcd54
Gina Turco <gturco@uci.edu>
2011-06-12 00:34:08 -0700

    spelling issue

--- pipeline/scripts/create_json.py
+++ pipeline/scripts/create_json.py
@@ -7,7 +7,7 @@ def create_json(query,subject):
   "fasta":"data/{0}_{1}/{0}.fasta".format(query,subject)},"genome_b": {"name":"{0}".format(subject),"flat":"data/{0}_{1}/{1}.bed".format(query,subject),
   "fasta":"data/{0}_{1}/{1}.fasta".format(query,subject)},"blast": {"W": 20,"a": 8,"e": 0.001},
   "defult ": {"blast_path":"/user/bin/blastall","out_dir":"data/{0}_{1}/".format(query,subject),"min_len":100, "reciprocal":True,"blast_log":True}}
-  json_fomatted = json.dumps(json_dict,indent=4)
+  json_formatted = json.dumps(json_dict,indent=4)
   json_file.write(json_formatted)
 
 if __name__ == "__main__":
@@ -16,6 +16,6 @@ if __name__ == "__main__":
     parser.add_option("--query", dest="query", help="name of query org or ORGA")
     parser.add_option("--subject", dest="subject", help="name of subject org or ORGB")
     (options, _) = parser.parse_args()
-    
-    create_json(options.query, options.subject)
+    create_json('options.query', 'options.subject')
+    #create_json(options.query, options.subject)
 
