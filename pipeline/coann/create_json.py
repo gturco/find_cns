6d9263bf81e2d5526ffe6f4cf980b1a8cd257bf0
Gina Turco <gturco88@gmail.com>
2011-12-09 14:23:10 -0800

    added run.sh and json

---
+++ pipeline/coann/create_json.py
@@ -0,0 +1,22 @@
+import json
+
+def create_json(query,subject,blast):
+  "formats my file information into a json file needed for coannot"
+  json_file = open("data/{0}_{1}/{0}_{1}.json".format(query,subject), "w")
+  json_dict = {"genome_a": {"name":"{0}".format(query),"flat":"data/{0}_{1}/{0}.bed".format(query,subject),
+  "fasta":"data/{0}_{1}/{0}.fasta".format(query,subject)},"genome_b": {"name":"{0}".format(subject),"flat":"data/{0}_{1}/{1}.bed".format(query,subject),
+  "fasta":"data/{0}_{1}/{1}.fasta".format(query,subject)},"blast": {"W": 20,"a": 8,"e": 0.001},
+  "default": {"blast_path":"{0}/blastall".format(blast),"out_dir":"data/{0}_{1}/".format(query,subject),"min_len":100, "reciprocal":True,"blast_log":True}}
+  json_formatted = json.dumps(json_dict,indent=4)
+  json_file.write(json_formatted)
+
+if __name__ == "__main__":
+    import optparse
+    parser = optparse.OptionParser()
+    parser.add_option("--query", dest="query", help="name of query org or ORGA")
+    parser.add_option("--subject", dest="subject", help="name of subject org or ORGB")
+    parser.add_option("--blast_path",dest="blast", help="location of most up todate blast dir")    
+    (options, _) = parser.parse_args()
+
+    create_json(options.query, options.subject,options.blast)
+
---
+++ pipeline/coann/run.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+BLAST_DIR=~/src/blast-2.2.25/bin/
+#on new synteny, run these. and put .bed and .fasta files in data/ directory.
+#  perl export_to_bed.pl -fasta_name rice_v6.fasta -dsg 8163 -name_re "^Os\d\dg\d{5}$" > rice_v6.bed
+# perl export_to_bed.pl -fasta_name sorghum_v1.4.fasta -dsg 95 > sorghum_v1.4.bed
+
+# perl export_to_bed.pl -fasta_name brachy_v1.fasta -dsg 8120 > brachy_v1.bed
+# use OrganismView in CoGe to look up other dsgs.
+
+
+ORGA=rice_v6
+ORGB=sorghum_v1
+QUOTA=1:1
+NCPU=8
+#############################################
+# dont edit below here
+#############################################
+DIR=data/${ORGA}_${ORGB}/
+
+python create_json.py \
+      --query $ORGA \
+      --subject $ORGB \
+      --blast_path $BLAST_DIR
+coannotate.py $DIR/${ORGA}_${ORGB}.json
+python scripts/merge.py \
+        --missed_bed ${DIR}/missed_${ORGA}_from_${ORGB}.bed \
+        --missed_matches ${DIR}/missed_${ORGA}_from_${ORGB}.matches.txt \
+        --old_bed ${DIR}/${ORGA}.bed \
+        --out ${DIR}/${ORGA}.all.bed
+python scripts/merge.py \
+        --missed_bed ${DIR}/missed_${ORGB}_from_${ORGA}.bed \
+        --missed_matches ${DIR}/missed_${ORGB}_from_${ORGA}.matches.txt \
+        --old_bed ${DIR}/${ORGB}.bed \
+        --out ${DIR}/${ORGB}.all.bed
