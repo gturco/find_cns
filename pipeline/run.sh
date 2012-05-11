d793af6486bf4213e40c283154ace83ac4d7f32b
gturco <gturco@berkeley.edu>
2012-05-11 15:50:24 -0700

    working run.sh if co-anno is ran

--- pipeline/run.sh
+++ pipeline/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-BLAST_DIR=~/blast-2.2.25/bin/
+BLAST_DIR=/home/gturco/src/blast-2.2.25/bin/
 #on new synteny, run these. and put .bed and .fasta files in data/ directory.
 #  perl export_to_bed.pl -fasta_name rice_v6.fasta -dsg 8163 -name_re "^Os\d\dg\d{5}$" > rice_v6.bed
 # perl export_to_bed.pl -fasta_name sorghum_v1.4.fasta -dsg 95 > sorghum_v1.4.bed
@@ -8,8 +8,8 @@ BLAST_DIR=~/blast-2.2.25/bin/
 # use OrganismView in CoGe to look up other dsgs.
 
 
-ORGA=rice_j
-ORGB=sorghum_n
+ORGA=sorghum_v1
+ORGB=brachy
 QUOTA=1:1
 NCPU=8
 #### look on coge for dsgid information
@@ -34,25 +34,38 @@ python scripts/find_cns.py \
         --qpad 12000 \
         --spad 12000 \
         --blast_path ${BLAST_DIR}/bl2seq \
-        --pair_fmt pair > $DIR/${ORGA}_${ORGB}.cns.txt
+        --pair_fmt pair > $DIR/${ORGA}_${ORGB}.cns.txt 
+
+python scripts/localdup.py \
+       -q $DIR/${ORGA}.fasta --qbed $DIR/${ORGA}.all.bed \
+       -s $DIR/${ORGB}.fasta --sbed $DIR/${ORGB}.all.bed \
+        -p $DIR/${ORGA}_${ORGB}.pairs.txt \
+       --cns_file $DIR/${ORGA}_${ORGB}.cns.txt \
+        -F T \
+        -n 8 \
+        --qpad 12000 \
+        --spad 12000 \
+        --blast_path ${BLAST_DIR}/bl2seq \
+        --pair_fmt pair \
+       --qdups $DIR/${ORGA}.all.localdups \
+       --sdups $DIR/${ORGB}.all.localdups
 
 python scripts/post_processing/cns_to_fasta.py \
-                -c $DIR/${ORGA}_${ORGB}.cns.txt \
+                -c $DIR/${ORGA}_${ORGB}.cns.txt.local \
                 --qfasta $DIR/${ORGA}.genomic.masked.fasta \
                 --sfasta $DIR/${ORGB}.genomic.masked.fasta \
                 --qorg ${ORGA} \
                 --sorg ${ORGB} \
                 --min_len=18 \
                 > $DIR/${ORGA}_${ORGB}.cns.fasta
-#echo removing cns that have hits in arabidopsis as rna or protein
-#wget -O data/at_protein.fasta ftp://ftp.arabidopsis.org/home/tair/Sequences/blast_datasets/TAIR10_blastsets/TAIR10_pep_20101214
-#wget -O data/os_protein.fasta ftp://ftp.plantbiology.msu.edu/pub/data/Eukaryotic_Projects/o_sativa/annotation_dbs/pseudomolecules/version_6.1/all.dir/all.pep
+##echo removing cns that have hits in arabidopsis as rna or protein
+##wget -O data/at_protein.fasta ftp://ftp.arabidopsis.org/home/tair/Sequences/blast_datasets/TAIR10_blastsets/TAIR10_pep_20101214
+##wget -O data/os_protein.fasta ftp://ftp.plantbiology.msu.edu/pub/data/Eukaryotic_Projects/o_sativa/annotation_dbs/pseudomolecules/version_6.1/all.dir/all.pep
 #bblast.py -p blastx -d data/at_protein.fasta -i $DIR/${ORGA}_${ORGB}.cns.fasta -e 0.01 -m 8 -a ${NCPU} -o $DIR/at_protein.blast
 #bblast.py -p blastx -d data/os_protein.fasta -i $DIR/${ORGA}_${ORGB}.cns.fasta -e 0.01 -m 8 -a ${NCPU} -o $DIR/os_protein.blast
 #
 #python scripts/post_processing/find_exons.py \
-#                 -q ${ORGA}\
-#                 -s ${ORGB}\
+#                 --cns $DIR/${ORGA}_${ORGB}.cns.txt.local \
 #                 -o $DIR \
 #                 $DIR/at_protein.blast $DIR/os_protein.blast
 #
@@ -74,37 +87,43 @@ python scripts/post_processing/cns_to_fasta.py \
 #   	 -o $DIR \
 #   	 -d data/thaliana_v9.description
 ##
-##python scripts/post_processing/shuffle_protein_cns.py \
-#    --qbed $DIR/${ORGA}.nolocaldups.bed \
-#    --sbed $DIR/${ORGB}.nolocaldups.bed \
-#    --cns  $DIR/${ORGA}_${ORGB}.cns.txt \
-#    --paralogy  $DIR/${ORGA}_${ORGB}.raw.filtered \
-#    --orthology $DIR/${ORGA}_${ORGB}.raw.filtered \
-#  # creates: $DIR/${ORGA}_${ORGB}.quota.with_new.orthology
+#cp $DIR/${ORGA}_${ORGB}.raw.filtered.local  $DIR/${ORGA}_${ORGB}.raw2.filtered.local
+
+#python scripts/post_processing/shuffle_protein_cns.py \
+#    --qbed $DIR/${ORGA}.nolocaldups.bed.local \
+#    --sbed $DIR/${ORGB}.nolocaldups.bed.local \
+#    --cns  $DIR/${ORGA}_${ORGB}.cns.txt.local \
+#    --paralogy  $DIR/${ORGA}_${ORGB}.raw.filtered.local \
+#    --orthology $DIR/${ORGA}_${ORGB}.raw2.filtered.local \
+#    --pairs $DIR/${ORGA}_${ORGB}.pairs.txt.local \ 
+# creates: $DIR/${ORGA}_${ORGB}.quota.with_new.orthology
 #
+
+
 #python scripts/post_processing/assign.py \
-#      --qbed $DIR/${ORGA}.nolocaldups.bed \
-#      --sbed $DIR/${ORGB}.nolocaldups.bed \
-#      --cns $DIR/${ORGA}_${ORGB}.cns.txt \
-#      --pairs $DIR/${ORGA}_${ORGB}.pairs.txt \
-#      --qdsid 9109 \
-#      --sdsid 95 \
+#      --qbed $DIR/${ORGA}.nolocaldups.bed.with_new.local \
+#      --sbed $DIR/${ORGB}.nolocaldups.bed.with_new.local \
+#      --cns $DIR/${ORGA}_${ORGB}.cns.txt.real.local \
+#      --pairs $DIR/${ORGA}_${ORGB}.pairs.txt.local \
+#      --qdsgid 11821 \
+#      --sdsgid 8120 \
 #      --qpad 15000 \
 #      --spad 15000 \
-#      --pair_fmt pair > $DIR/${ORGA}_${ORGB}.cns.assigned.csv
+#      --pair_fmt pair > $DIR/${ORGA}_${ORGB}.cns.assigned.csv.local
 
+#
 python scripts/post_processing/assign.py \
-      --qbed $DIR/${ORGA}.nolocaldups.bed \
-      --sbed $DIR/${ORGB}.nolocaldups.bed \
-      --cns $DIR/${ORGA}_${ORGB}.cns.txt \
-      --pairs $DIR/${ORGA}_${ORGB}.pairs.txt \
+      --qbed $DIR/${ORGA}.nolocaldups.bed.local \
+      --sbed $DIR/${ORGB}.nolocaldups.bed.local \
+      --cns $DIR/${ORGA}_${ORGB}.cns.txt.local \
+      --pairs $DIR/${ORGA}_${ORGB}.pairs.txt.local \
       --qdsgid $qDSGID \
       --sdsgid $sDSGID\
       --qpad 15000 \
       --spad 15000 \
-      --pair_fmt pair > $DIR/${ORGA}_${ORGB}.cns.assigned.csv
+      --pair_fmt pair > $DIR/${ORGA}_${ORGB}.cns.assigned.csv.local
 
-
+#
 #python scripts/cns_to_fasta.py \
 #                -c $DIR/${ORGA}_${ORGB}.cns.real.txt \
 #                --qfasta $DIR/${ORGA}.genomic.masked.fasta \
