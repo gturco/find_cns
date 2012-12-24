1d30536ed58567687058af2b2d837800d9af18f2
Gina Turco <gturco88@gmail.com>
2012-12-23 20:38:10 -0600

    added fasta parser to gff to be

--- pipeline/scripts/gff_to_bed.py
+++ pipeline/scripts/gff_to_bed.py
@@ -51,8 +51,21 @@ def parse_gff(regex,gff):
             parents[pid] = values
     return parents, feats
 
-def gff_to_bed(parents,feats,bed):
-    bed_fh = open(bed,"wb")
+def parse_fasta(fasta,out):
+    i = 0
+    no_i = []
+    filter_fasta = open("{0}.fasta".format(out),"wb")
+    for line in open(fasta):
+        i += 1
+        if line[:4] == "gi|M" or line[:4] == "gi|C":
+            no_i.append(i + 1)
+            continue
+        if i in no_i: continue
+        filter_fasta.write(line)
+
+ 
+def gff_to_bed(parents,feats,out):
+    bed_fh = open("{0}.bed".format(out),"wb")
     for pid in parents.keys():
         seqid,start,end,strand,accn = parents[pid]
         largest_feats = merge_feats(feats[pid])
@@ -62,17 +75,19 @@ def gff_to_bed(parents,feats,bed):
                 strand,len(largest_feats),",".join(feat_lens),",".join(feat_starts))
         bed_fh.write(bed_line)
 
-def main(regex,gff,bed):
+def main(regex,gff,fasta,out):
     parents,feats = parse_gff(regex,gff)
-    gff_to_bed(parents,feats,bed)
+    gff_to_bed(parents,feats,out)
+    parse_fasta(fasta,out)
 
 if __name__ == "__main__":
     import optparse
     parser = optparse.OptionParser("usage: %prog [options] ")
     parser.add_option("--re", dest="regex", default="This_will_not_be_found",  help="regex expression for gene name")
     parser.add_option("--gff", dest="gff", help="gff file from CoGe")
-    parser.add_option("--bed", dest="bed", help="name of bed file output")
+    parser.add_option("--fasta", dest="fasta", help="fasta file from CoGe")
+    parser.add_option("--out", dest="out", help="root names for bed and fasta file output")
     (options, _) = parser.parse_args()
 
-    main(options.regex,options.gff,options.bed)
+    main(options.regex,options.gff,options.fasta,options.out)
 
